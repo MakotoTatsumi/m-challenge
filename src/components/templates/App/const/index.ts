47946f0af3c5146d1437e91dab9c740c5a1e8f93
MakotoTatsumi <sakuchan.jp@gmail.com>
2022-04-04 00:50:13 +0900

    add: ORDERLISTの値を定義

---
+++ src/components/templates/App/const/index.ts
@@ -0,0 +1,28 @@
+export const ORDER_LIST = [
+  {
+    id: "coffee",
+    label: "コーヒー",
+    price: 480,
+  },
+  {
+    id: "tea",
+    label: "紅茶",
+    price: 280,
+  },
+  {
+    id: "milk",
+    label: "ミルク",
+    price: 180,
+  },
+  {
+    id: "coke",
+    label: "コーラ",
+    price: 190,
+  },
+  {
+    id: "beer",
+    label: "ビール",
+    price: 580,
+  },
+] as const
+
