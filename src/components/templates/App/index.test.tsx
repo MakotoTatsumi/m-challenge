341654211c61aa3d5004f26cae65fadf268e0562
MakotoTatsumi <sakuchan.jp@gmail.com>
2022-04-04 01:05:06 +0900

    fix: React18対応

--- src/components/templates/App/index.test.tsx
+++ src/components/templates/App/index.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { App } from '.';
+import React from "react"
+import { fireEvent, render, screen } from "@testing-library/react"
+import { App } from "."
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("Count", () => {
+  render(<App />)
+  const countElement = screen.getByTestId("count")
+  fireEvent.click(screen.getByTestId("coffee"))
+  expect(countElement).toHaveTextContent("1")
+})
